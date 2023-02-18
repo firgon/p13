84482b08f87acb249215072682ebe86e9c0028ed
firgon <emmanuel.albisser@gmail.com>
2023-02-18 13:58:59 +0100

    all tests

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+import pytest
+from django.test import Client
+from django.urls import reverse
 
-# Create your tests here.
+from lettings.models import Letting
+from oc_lettings_site import tests
+from oc_lettings_site.tests import convert_to_title
+
+
+class TestIndex(tests.TestWebPage):
+    url = 'lettings_index'
+    expected_content = "Lettings"
+
+
+class TestDetail:
+    base_url = 'letting'
+    model = Letting
+    letting_id = 2
+
+    @pytest.mark.django_db
+    def test_page(self):
+        client = Client()
+
+        path = reverse(self.base_url,  kwargs={'letting_id': self.letting_id})
+        print(path)
+        response = client.get(path)
+        content = response.content.decode()
+
+        expected_content = convert_to_title(
+            self.model.Objects.get(pk=self.letting_id).title)
+
+        assert response.status_code == 200
+        assert expected_content in content
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,23 @@
-def test_dummy():
-    assert 1
+import pytest
+from django.test import Client
+from django.urls import reverse
+
+
+def convert_to_title(title: str)-> str:
+    return f"<h1>{title}</h1>"
+
+
+class TestWebPage:
+    url = 'index'
+    expected_content = "Welcome to Holiday Homes"
+
+    @pytest.mark.django_db
+    def test_page(self):
+        client = Client()
+
+        path = reverse(self.url)
+        response = client.get(path)
+        content = response.content.decode()
+        assert response.status_code == 200
+        assert convert_to_title(self.expected_content) in content
+
