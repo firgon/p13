c8cb162cdf9d85a7afece6bf396a7673f69f32b1
firgon <firgon@hotmail.com>
2023-03-03 15:09:17 +0100

    test

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,8 +13,8 @@ IS_HEROKU = "DYNO" in os.environ
 SECRET_KEY = os.environ.get('SECRET_KEY', default='foo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU:
-    DEBUG = True
+# if not IS_HEROKU:
+DEBUG = True
 
 # Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
 if IS_HEROKU:
